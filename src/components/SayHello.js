e99aa969da349a28ff23c41c1d8ef0b38102e65e
Alyssa Cleland <alyssamariecleland@gmail.com>
2024-12-26 22:15:15 -0500

    learned about props

--- src/components/SayHello.js
+++ src/components/SayHello.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export default function SayHello({ name }) {
+export default function SayHello({ name, greeting }) {
+  // could also pass in (props) and then access props.name below, because props is an object
   return (
-    <>
-    <div>Hello, {name}</div>
-    <p>This is a paragraph</p>
-    </>
+  
+    <div>Hello, {name}. {greeting}
+
+    </div>
+
+
   )
 }
-//
-// this exists
+
 SayHello.propTypes = {
   name: PropTypes.string.isRequired,
+  greeting: PropTypes.string.isRequired,
 }
